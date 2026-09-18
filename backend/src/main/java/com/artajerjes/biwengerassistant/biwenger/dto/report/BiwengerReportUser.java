1882ee7f7f1f3dad5c54b19f68a33ff03e64dfec
DiegoGoujonG <diego.goujong@ies.onmicrosoft.com>
2026-09-18 10:32:42 +0200

    chore: clean up backend dead code

--- backend/src/main/java/com/artajerjes/biwengerassistant/biwenger/dto/report/BiwengerReportUser.java
+++
@@ -1,7 +0,0 @@
-package com.artajerjes.biwengerassistant.biwenger.dto.report;
-
-public record BiwengerReportUser(
-        Long id,
-        String name,
-        String icon) {
-}
--- backend/src/main/java/com/artajerjes/biwengerassistant/movement/MovementBidRepository.java
+++
@@ -1,7 +0,0 @@
-package com.artajerjes.biwengerassistant.movement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovementBidRepository
-        extends JpaRepository<MovementBid, Long> {
-}
